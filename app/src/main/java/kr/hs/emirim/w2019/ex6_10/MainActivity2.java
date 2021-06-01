78f7dff21641c2c783f5c2e5a63fdaed39ba4500
choi ji won <w2019@e-mirim.hs.kr>
2021-06-01 20:38:57 +0900

    Completion commit

---
+++ app/src/main/java/kr/hs/emirim/w2019/ex6_10/MainActivity2.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.w2019.ex6_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
